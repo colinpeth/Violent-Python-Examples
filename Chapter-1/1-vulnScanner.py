690cd91b9c5af530d8e7ae73407957c15867dcbd
colinpeth <43682220+colinpeth@users.noreply.github.com>
2020-10-06 09:06:45 -0400

    Update 1-vulnScanner.py
    Added comments in order to improve readability. Also allows users to easily implement features into other packages.

--- Chapter-1/1-vulnScanner.py
+++ Chapter-1/1-vulnScanner.py
@@ -4,7 +4,7 @@ import socket
 import os
 import sys
 
-
+#Connects to socket and trys to get banner
 def retBanner(ip, port):
     try:
         socket.setdefaulttimeout(2)
@@ -15,9 +15,9 @@ def retBanner(ip, port):
     except:
         return
 
-
+#Checkes for vulnerabilities 
 def checkVulns(banner, filename):
-
+    #Opens file and reads lines.
     f = open(filename, 'r')
     for line in f.readlines():
         if line.strip('\n') in banner:
@@ -26,14 +26,16 @@ def checkVulns(banner, filename):
 
 
 def main():
-
+    #Checks System arguments for filename
     if len(sys.argv) == 2:
         filename = sys.argv[1]
+        
+        #Checks if file exists
         if not os.path.isfile(filename):
             print '[-] ' + filename +\
                 ' does not exist.'
             exit(0)
-
+        #Checkes if access is allowed file
         if not os.access(filename, os.R_OK):
             print '[-] ' + filename +\
                 ' access denied.'
@@ -43,11 +45,15 @@ def main():
             ' <vuln filename>'
         exit(0)
 
+    #Runs port scanning to see if ip is available 
     portList = [21,22,25,80,110,443]
+    #Loops through IP addressed
     for x in range(147, 150):
-        ip = '192.168.95.' + str(x)
+        ip = '192.168.95.' + str(x) #Adds ending of IP
+        #Loops through port lisr
         for port in portList:
             banner = retBanner(ip, port)
+            #Prints Banner if avaibale
             if banner:
                 print '[+] ' + ip + ' : ' + banner
                 checkVulns(banner, filename)
